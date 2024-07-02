0f97badf6dacdfc7bfbf2b57305f73a8e0654935
JetWen <killer500k@gmail.com>
2024-07-02 11:22:32 +0800

    Add dlib tools in the Project

--- src/24_Face_Feature_detection.py
+++ src/24_Face_Feature_detection.py
@@ -1,72 +1,79 @@
-# 載入相關套件
 import dlib
 import cv2
-import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle, Circle
 from imutils import face_utils
+import matplotlib.pyplot as plt
 
-# 載入圖檔
-image_file = "/home/Jet/OpenCV/Images/two.jpg"
-image = plt.imread(image_file)
+# 載入圖像檔案
+image_file = "./images_face/jared_1.jpg"
+image = cv2.imread(image_file)
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # 轉換顏色通道為 RGB
 
 # 顯示圖像
-plt.imshow(image)
+plt.imshow(image_rgb)
 plt.axis('off')
-plt.savefig('1.png')
 
 
-# 載入 dlib 以 HOG 基礎的臉部偵測模型
-model_file = "shape_predictor_68_face_landmarks.dat"
+# 載入 dlib 的人臉偵測模型和特徵點預測模型
+model_file = "./shape_predictor_68_face_landmarks.dat"
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(model_file)
 
-# 偵測圖像的臉部
-rects = detector(image)
+# 偵測圖像中的人臉
+rects = detector(image_rgb)
 
-print(f'偵測到{len(rects)}張臉部.')
-# 偵測每張臉的特徵點
+print(f'偵測到{len(rects)}張臉.')
+
+# 遍歷每張偵測到的人臉
 for (i, rect) in enumerate(rects):
     # 偵測特徵點
-    shape = predictor(image, rect)
-    
-    # 轉為 NumPy 陣列
-    shape = face_utils.shape_to_np(shape)
+    shape = predictor(image_rgb, rect)
+    shape = face_utils.shape_to_np(shape)  # 轉為 NumPy 陣列
 
-    # 標示特徵點
+    # 標記特徵點
     for (x, y) in shape:
-        cv2.circle(image, (x, y), 10, (0, 255, 0), -1)
-        
-# 顯示圖像
-plt.imshow(image)
+        cv2.circle(image_rgb, (x, y), 10, (0, 255, 0), -1)
+
+# 顯示帶有特徵點的圖像
+plt.imshow(image_rgb)
 plt.axis('off')
-plt.savefig('2.png')
+plt.savefig('feature_Point.png')
+print("-" * 10, "特徵點標記完成", "-" * 10)
 
 
-# 讀取視訊檔
-cap = cv2.VideoCapture('./images_face/hamilton_clip.mp4')
+# 讀取視訊檔案
+cap = cv2.VideoCapture('./images_face/short_hamilton_clip.mp4')
+
 while True:
-    # 讀取一幀影像
-    _, image = cap.read()
+    # 讀取一幀圖像
+    ret, image = cap.read()
+    if not ret:
+        break
     
-    # 偵測圖像的臉部
-    rects = detector(image)    
+    # 轉換為灰度圖像，用於人臉偵測
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # 偵測圖像中的人臉
+    rects = detector(gray)
+    
+    # 遍歷每張偵測到的人臉
     for (i, rect) in enumerate(rects):
         # 偵測特徵點
-        shape = predictor(image, rect)
-        shape = face_utils.shape_to_np(shape)
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)  # 轉為 NumPy 陣列
     
-        # 標示特徵點
+        # 標記特徵點
         for (x, y) in shape:
             cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
     
-    # 顯示影像
+    # 顯示圖像
     cv2.imshow("Output", image)
 
-    k = cv2.waitKey(5) & 0xFF    # 按 Esc 跳離迴圈
+    # 按 Esc 鍵退出迴圈
+    k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
 
-# 關閉輸入檔    
+# 關閉視訊檔案
 cap.release()
 # 關閉所有視窗
 cv2.destroyAllWindows()
---
+++ src/27_ANPR_ColabVer.py
@@ -0,0 +1,121 @@
+# 載入相關套件
+!sudo apt-get install tesseract-ocr
+!sudo apt-get install tesseract-ocr-chi-tra
+!pip3 install pytesseract
+import cv2
+import imutils
+import numpy as np
+import matplotlib.pyplot as plt
+import pytesseract
+from PIL import Image
+from google.colab.patches import cv2_imshow
+
+# 載入圖檔
+image = cv2.imread('./2.jpg',cv2.IMREAD_COLOR)
+
+# 顯示圖檔
+image_RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+plt.imshow(image_RGB)
+plt.axis('off')
+plt.show()
+
+# 萃取輪廓
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # 轉為灰階
+gray = cv2.bilateralFilter(gray, 11, 17, 17)   # 模糊化，去除雜訊
+edged = cv2.Canny(gray, 30, 200)               # 萃取輪廓
+
+# 顯示圖檔
+plt.imshow(edged, cmap='gray')
+plt.axis('off')
+plt.show()
+
+# 取得等高線區域，並排序，取前10個區域
+cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+
+cnts[0].shape
+
+# 找第一個含四個點的等高線區域
+screenCnt = None
+for i, c in enumerate(cnts):
+    # 計算等高線區域周長
+    peri = cv2.arcLength(c, True)
+    # 轉為近似多邊形
+    approx = cv2.approxPolyDP(c, 0.018 * peri, True)
+    # 等高線區域維度
+    print(c.shape)
+
+    # 找第一個含四個點的多邊形
+    if len(approx) == 4:
+        screenCnt = approx
+        print(i)
+        break
+
+# 在原圖上繪製多邊形，框住車牌
+if screenCnt is None:
+    detected = 0
+    print("No contour detected")
+else:
+    detected = 1
+    
+if detected == 1:
+    cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3)
+    print(f'車牌座標=\n{screenCnt}')
+
+# 去除車牌以外的圖像
+mask = np.zeros(gray.shape,np.uint8)
+new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
+new_image = cv2.bitwise_and(image, image, mask=mask)
+
+# 轉為浮點數
+src_pts = np.array(screenCnt, dtype=np.float32)
+
+# 找出車牌的上下左右的座標
+left = min([x[0][0] for x in src_pts])
+right = max([x[0][0] for x in src_pts])
+top = min([x[0][1] for x in src_pts])
+bottom = max([x[0][1] for x in src_pts])
+
+# 計算車牌寬高
+width = right - left  
+height = bottom - top 
+print(f'寬度={width}, 高度={height}')
+
+# 計算仿射(affine transformation)的目標區域座標，須與擷取的等高線區域座標順序相同
+if src_pts[0][0][0] > src_pts[1][0][0] and src_pts[0][0][1] < src_pts[3][0][1]:
+    print('起始點為右上角')
+    dst_pts = np.array([[width, 0], [0, 0], [0, height], [width, height]], dtype=np.float32)
+elif src_pts[0][0][0] < src_pts[1][0][0] and src_pts[0][0][1] > src_pts[3][0][1]:
+    print('起始點為左下角')
+    dst_pts = np.array([[0, height], [width, height], [width, 0], [0, 0]], dtype=np.float32)
+else:
+    print('起始點為左上角')
+    dst_pts = np.array([[0, 0], [0, height], [width, height], [width, 0]], dtype=np.float32)
+    
+# 仿射
+M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+Cropped = cv2.warpPerspective(gray, M, (int(width), int(height)))
+
+# 車牌號碼 OCR 辨識
+text = pytesseract.image_to_string(Cropped, config='--psm 11')
+print("車牌號碼：",text)
+
+# 顯示原圖及車牌
+cv2_imshow(image)  # 使用 cv2_imshow() 替代 cv2.imshow()
+cv2_imshow(Cropped)  # 使用 cv2_imshow() 替代 cv2.imshow()
+
+# 車牌存檔
+cv2.imwrite('Cropped.jpg', Cropped)
+
+# 按 Enter 鍵結束
+cv2.waitKey(0)
+
+# 關閉所有視窗
+cv2.destroyAllWindows()
+
+
+
+
+
+
